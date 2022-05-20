0a9b6541b279e069604c6075a63a1e6d5d477a8c
Thomas Uhlig <th.uhlig@googlemail.com>
2022-05-20 22:30:52 +0200

    added some kind of a menu

--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@ const readline = ReadLine.createInterface({
   output: process.stdout,
 });
 
-readline.question(`Choose your weapon [a-zA-Z]: `, letter => {
-  console.log(`You have chosen: ${letter}!`);
-  readline.close();
+const diamond = () => {
+  readline.question(`Choose your weapon [a-zA-Z]: `, letter => {
+    console.log(`You have chosen: ${letter}!`)
+    readline.close()
 
-  shineBrightLikeADiamond(letter)
-});
+    shineBrightLikeADiamond(letter)
+  })
+}
+
+const katas = {
+  "1" : [ "Diamond", diamond ]
+}
+
+for (let i in katas)
+  console.log(`${i} -> ${katas[i][0]}`)
+
+readline.question(`Choose a Kata (${Object.keys(katas).join(", ")}): `, option => {
+  katas[option][1]()
+})
